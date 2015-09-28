301e6056a5e96311c824c6e335f8d0a73dd113e5
Gregor Riepl <onitake@gmail.com>
2015-09-28 18:43:37 +0200

    Added i18n repo to get script Prefixed submodule paths (kicad-)

--- debian/get-kicad.sh
+++ debian/get-kicad.sh
@@ -9,13 +9,15 @@ origtarxz=kicad_$version.orig.tar.xz
 
 echo "========== Getting the stable core ======================="
 bzr export -r $revision $package lp:kicad/4.0
-echo "========== Getting the documents ========================="
+echo "========== Getting the UI translations ==================="
 # TODO: Instead of cloning, we may be able to use git archive --remote <url> <sha1>
-git clone https://github.com/KiCad/kicad-doc $package/doc
+git clone https://github.com/KiCad/kicad-i18n $package/kicad-i18n
+echo "========== Getting the documents ========================="
+git clone https://github.com/KiCad/kicad-doc $package/kicad-doc
 echo "========== Getting the module and symbol libraries ======="
-git clone https://github.com/KiCad/kicad-library $package/library
+git clone https://github.com/KiCad/kicad-library $package/kicad-library
 echo "========== Getting the footprint libraries ==============="
-mkdir -p $package/pretty
+mkdir -p $package/kicad-pretty
 # Use github API to list repos for org KiCad, then subset the JSON reply for only
 # *.pretty repos in the "full_name" variable.
 PRETTY_REPOS=`curl https://api.github.com/orgs/KiCad/repos?per_page=2000 2> /dev/null \
@@ -25,15 +27,16 @@ SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 for pretty in $PRETTY_REPOS; do
 	echo "Getting: $pretty"
-	git clone https://github.com/KiCad/$pretty $package/pretty/$pretty
+	git clone https://github.com/KiCad/$pretty $package/kicad-pretty/$pretty
 done
 IFS=$SAVEIFS
 
 echo "========== Compressing the archive ======================="
 tar --create --xz --exclude=.git --file $origtarxz $package
 echo "Sizes:"
-echo "Core:       "$(du --exclude=doc --exclude=library --exclude=pretty -sh $package)
-echo "Doc:        "$(du --exclude=.git -sh $package/doc)
-echo "Library:    "$(du --exclude=.git -sh $package/library)
-echo "Footprints: "$(du --exclude=.git -sh $package/pretty)
+echo "Core:       "$(du --exclude=kicad-i18n --exclude=kicad-doc --exclude=kicad-library --exclude=kicad-pretty -sh $package)
+echo "I18n:       "$(du --exclude=.git -sh $package/kicad-i18n)
+echo "Doc:        "$(du --exclude=.git -sh $package/kicad-doc)
+echo "Library:    "$(du --exclude=.git -sh $package/kicad-library)
+echo "Footprints: "$(du --exclude=.git -sh $package/kicad-pretty)
 
